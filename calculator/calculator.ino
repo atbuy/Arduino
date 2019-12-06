ca769e6f1561528764f0549a25eb8b3bfd6671bb
Vitaman02 <56309299+Vitaman02@users.noreply.github.com>
2019-12-07 01:37:06 +0200

    Xlro

--- calculator/calculator.ino
+++ calculator/calculator.ino
@@ -58,13 +58,28 @@ void loop() {
       lcd.print("9");
       num += "9";
     }
-    else if (val == 43) {
-      lcd.print("+");
-      op = "+";
+    else if (val == 47 || val == 45 || val == 42 || val == 43) {
+      if (val == 43) {
+        op = "+";
+      }
+      else if (val == 42) {
+        op = "*";
+      }
+      else if (val == 47) {
+        op = "/";
+      }
+      else if (val == 45) {
+        op = "-";
+      }
+      lcd.print(op);
       while (val2 != 61) {
         val2 = Serial.read();
         delay(300);
-        if (val2 == 49) {
+        if (val2 == 48) {
+          lcd.print("0");
+          num2 += "0";
+        }
+        else if (val2 == 49) {
           lcd.print("1");
           num2 += "1";
         }
@@ -102,140 +117,7 @@ void loop() {
         }
       }
     }
-    else if (val == 45) {
-      lcd.print("-");
-      op = "-";
-      while (val2 != 61) {
-        val2 = Serial.read();
-        delay(300);
-        if (val2 == 49) {
-          lcd.print("1");
-          num2 += "1";
-        }
-        else if (val2 == 50) {
-          lcd.print("2");
-          num2 += "2";
-        }
-        else if (val2 == 51) {
-          lcd.print("3");
-          num2 += "3";
-        }
-        else if (val2 == 52) {
-          lcd.print("4");
-          num2 += "4";
-        }
-        else if (val2 == 53) {
-          lcd.print("5");
-          num2 += "5";
-        }
-        else if (val2 == 54) {
-          lcd.print("6");
-          num2 += "6";
-        }
-        else if (val2 == 55) {
-          lcd.print("7");
-          num2 += "7";
-        }
-        else if (val2 == 56) {
-          lcd.print("8");
-          num2 += "8";
-        }
-        else if (val2 == 57) {
-          lcd.print("9");
-          num2 += "9";
-        }
-      }
-    }
-    else if (val == 47) {
-      lcd.print("/");
-      op = "/";
-      while (val2 != 61) {
-        val2 = Serial.read();
-        delay(300);
-        if (val2 == 49) {
-          lcd.print("1");
-          num2 += "1";
-        }
-        else if (val2 == 50) {
-          lcd.print("2");
-          num2 += "2";
-        }
-        else if (val2 == 51) {
-          lcd.print("3");
-          num2 += "3";
-        }
-        else if (val2 == 52) {
-          lcd.print("4");
-          num2 += "4";
-        }
-        else if (val2 == 53) {
-          lcd.print("5");
-          num2 += "5";
-        }
-        else if (val2 == 54) {
-          lcd.print("6");
-          num2 += "6";
-        }
-        else if (val2 == 55) {
-          lcd.print("7");
-          num2 += "7";
-        }
-        else if (val2 == 56) {
-          lcd.print("8");
-          num2 += "8";
-        }
-        else if (val2 == 57) {
-          lcd.print("9");
-          num2 += "9";
-        }
-      }
-    }
-    else if (val == 42) {
-      lcd.print("*");
-      op = "*";
-      while (val2 != 61) {
-        val2 = Serial.read();
-        delay(300);
-        if (val2 == 49) {
-          lcd.print("1");
-          num2 += "1";
-        }
-        else if (val2 == 50) {
-          lcd.print("2");
-          num2 += "2";
-        }
-        else if (val2 == 51) {
-          lcd.print("3");
-          num2 += "3";
-        }
-        else if (val2 == 52) {
-          lcd.print("4");
-          num2 += "4";
-        }
-        else if (val2 == 53) {
-          lcd.print("5");
-          num2 += "5";
-        }
-        else if (val2 == 54) {
-          lcd.print("6");
-          num2 += "6";
-        }
-        else if (val2 == 55) {
-          lcd.print("7");
-          num2 += "7";
-        }
-        else if (val2 == 56) {
-          lcd.print("8");
-          num2 += "8";
-        }
-        else if (val2 == 57) {
-          lcd.print("9");
-          num2 += "9";
-        }
-      }
-    }
-    
-    if (val == 61) {
+    else if (val == 61) {
       long num_ = num.toInt();
       long num2_ = num2.toInt();
       if (op == "+") {
@@ -255,6 +137,5 @@ void loop() {
       Serial.println(tot);
       delay(2000);
     }
-    //Serial.println(val);
 
 }
